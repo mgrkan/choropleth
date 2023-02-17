794457756e87ef57d18e9914eccd458030bb9a76
mgrkan <79905545+mgrkan@users.noreply.github.com>
2023-02-17 09:40:22 +0300

    Delete bar.py

--- bar.py
+++
@@ -1,6 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-df = pd.read_csv("./tr_number_of_storeys.csv")
-fig = px.bar(df, y="number_of_storeys",x ="city", title="Average number of storeys in a building per city")
-fig.show()
